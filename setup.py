6fe128fb10b650b725ed9d5f0d6bbb58c054ee60
Christian Oudard <christian.oudard@gmail.com>
2013-01-07 17:25:09 -0800

    Bumped minor version number.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='fabutil',
-      version='0.5.2',
+      version='0.6.0',
       description='Helper functions for Fabric deployments.',
       author='TinyCo',
       author_email='backend@tinyco.com',
