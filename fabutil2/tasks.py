410ea942c8b2698dcc73347b0de28d9d5823fb14
Christian Oudard <christian.oudard@gmail.com>
2013-01-14 18:31:55 -0800

    Created a submodule "fabutil2.tasks" to allow for easy import of fab tasks.

---
+++ fabutil2/tasks.py
@@ -0,0 +1,18 @@
+# Collecting common tasks here for easy import from fabfiles.
+from fabutil2 import (
+    update_code,
+    flip,
+    ping,
+    setup_base_system,
+    configure_nginx,
+    delete_mail_queue,
+    setup_mysql,
+    build_packages,
+    clone_from_config,
+    setup_user_account,
+    setup_user,
+    setup_user_runit,
+    configure_sd_plugin,
+    install_redis,
+    install_terrarium,
+)
