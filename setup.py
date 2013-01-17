c419953cbc1a1959fc2e7e6890e459d3ca21f666
Christian Oudard <christian.oudard@gmail.com>
2013-01-16 16:35:53 -0800

    Incremented version number.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='fabutil',
-      version='1.0.0',
+      version='1.0.1',
       description='Helper functions for Fabric deployments.',
       author='TinyCo',
       author_email='backend@tinyco.com',
