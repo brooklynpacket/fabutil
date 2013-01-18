b21c3df592da4fe0e9a4e0268286354d38299fca
Christian Oudard <christian.oudard@gmail.com>
2013-01-18 12:17:56 -0800

    No need to uninstall virtualenv anymore, we are not using a forked version.

--- fabutil2/__init__.py
+++ fabutil2/__init__.py
@@ -754,9 +754,8 @@ def install_terrarium():
     # This is a temporary fix. The correct way to do this is through a tool
     # such as puppet, but in the meantime, we need a quick way to get this up
     # and running on existing servers.
-    sudo('pip uninstall virtualenv')
-    sudo('pip install --upgrade virtualenv==1.8.4')
     sudo('pip install --upgrade pip')
+    sudo('pip install --upgrade virtualenv==1.8.4')
     sudo(
         'pip install --upgrade '
         '-e git+git://github.com/brooklynpacket/terrarium.git#egg=terrarium'
